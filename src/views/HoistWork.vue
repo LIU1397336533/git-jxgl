37b6d0e4024ac3b216eba4a918ee3052cd46c77d
914309809 <914309809@qq.com>
2020-12-14 14:38:54 +0800

    添加吊挂作业页面

---
+++ src/views/HoistWork.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="HoistWork">
+    <div class="header">
+      <div class="headleft">
+        <i class="icon iconfont icon-zuojiantou" @click="returnBefore"></i>
+        <span class="zi">机械档案</span>
+      </div>
+      <div class="headright">
+        <i class="icon iconfont icon-lingdang"></i>
+        <span class="dian">•</span>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="less">
+</style>
