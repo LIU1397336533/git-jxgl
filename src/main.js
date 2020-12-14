80702b2d38df03d991d45288235ad5630029fea1
1996wxj <1509527550@qq.com>
2020-12-14 15:41:27 +0800

    备注

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import 'lib-flexible/flexible.js'
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 import VueScroller from 'vue-scroller'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(VueScroller)
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 Vue.use(MintUI)
 
